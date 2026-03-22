d2dad26f04949afbc548fcf9b9aee4053156cb54
Atharva110409 <atharvagangurde12345@gmail.com>
2026-03-22 22:57:21 +0530

    Delete main.py

--- main.py
+++
@@ -1,6 +0,0 @@
-
-
-from src.train import train_model
-
-if __name__ == "__main__":
-    train_model(r"C:\Users\athar\OneDrive\Desktop\ML_Project\house price prediction\data\data.csv", target_column="price")
